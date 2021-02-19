99c36aaa43904271e93c1b940e7f607223cec368
Nanna Elowsson <nannaelowsson@MacBook-Pro-tilhrende-Nanna.local>
2021-02-19 14:37:45 +0100

    forsøg med script i singleview

---
+++ lande_singleview.js
@@ -0,0 +1,32 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+
+const header = document.querySelector("header");
+const medieurl = "https://justjourney-1298.restdb.io/media/";
+const myHeaders = {
+    "x-apikey": "602f73905ad3610fb5bb639c"
+}
+let lande;
+
+
+console.log("ID", id);
+document.addEventListener("DOMContentLoaded", loadJSON);
+
+async function loadJSON() {
+    console.log("loadJSON");
+    const JSONData = await fetch(`https://justjourney-1298.restdb.io/rest/destinationer/${id}`, {
+        headers: myHeaders
+    });
+    lande = await JSONData.json();
+
+    console.log("lande", lande);
+    visLande(lande);
+}
+
+function visLande(land) {
+    console.log("visLande");
+
+    document.querySelector(".billede").src = medieurl + land.billede;
+
+
+}
